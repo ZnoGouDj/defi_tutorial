49ff4b29892c54b7fd1a75f24c4ac288e08eb40d
ZnoGouDj <znogoud3@gmail.com>
2022-02-23 16:09:33 +0300

    Part 1

--- src/contracts/DaiToken.sol
+++ src/contracts/DaiToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.12;
 
 contract DaiToken {
     string  public name = "Mock DAI Token";
---
+++ src/contracts/TokenFarm.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.12;
+
+contract TokenFarm {
+    string public name = "Dapp Token Farm";
+}
+
