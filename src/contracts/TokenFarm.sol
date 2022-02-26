ceeca6ed1815630405ba9d5374e86b284740bda7
ZnoGouDj <znogoud3@gmail.com>
2022-02-26 19:27:51 +0300

    Part 2. Tests

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,17 @@
 pragma solidity ^0.8.12;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
+
 
